00c0ce4a58e082fec44f9d1e09ffe220fa703f9e
gram655 <terranysj@gmail.com>
2021-01-12 22:23:53 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="log" />
+      <h2>Let's develop mangement system!</h2>
+    </div>
+  );
+}
+
+export default App;
